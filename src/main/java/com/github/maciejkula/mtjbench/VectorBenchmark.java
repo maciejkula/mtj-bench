3b8d3f190487686007d02a21af8723810ffe222e
maciejkula <noocrat@gmail.com>
2014-06-04 18:07:44 +0100

    Initial commit.

---
+++ src/main/java/com/github/maciejkula/mtjbench/VectorBenchmark.java
@@ -0,0 +1,51 @@
+package com.github.maciejkula.mtjbench;
+
+import no.uib.cipr.matrix.DenseVector;
+import no.uib.cipr.matrix.Vector;
+import no.uib.cipr.matrix.sparse.SparseVector;
+import org.openjdk.jmh.annotations.*;
+
+/**
+ * Created by maciej on 04/06/14.
+ */
+@State(Scope.Thread)
+public class VectorBenchmark {
+
+    Vector xDense;
+    Vector yDense;
+
+    Vector xSparse;
+    Vector ySparse;
+
+    @Setup(Level.Iteration)
+    public void prepare() {
+        double[] x = {0.0, 1.0, 3.4, 15.2, 0.0, 0.0, 16.7, 0.0};
+        double[] y = {1.0, 2.3, 4.4, -3.2, 0.0, 1.3, 10.2, -4.0};
+
+        this.xDense = new DenseVector(x);
+        this.yDense = new DenseVector(y);
+
+        this.xSparse = new SparseVector(xDense);
+        this.ySparse = new SparseVector(yDense);
+    }
+
+    @GenerateMicroBenchmark
+    public void testDenseDot() {
+        xDense.dot(yDense);
+    }
+
+    @GenerateMicroBenchmark
+    public void testSparseSparseDot() {
+        xSparse.dot(ySparse);
+    }
+
+    @GenerateMicroBenchmark
+    public void testDenseSparseDot() {
+        xDense.dot(ySparse);
+    }
+
+    @GenerateMicroBenchmark
+    public void testSparseDenseDot() {
+        xSparse.dot(yDense);
+    }
+}
